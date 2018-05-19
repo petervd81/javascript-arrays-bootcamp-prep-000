ace0fb06c9cf7b502583bc0d157609cf764bc72c
Peter van Dijk <peter@petervandijk.net>
2018-05-19 16:10:21 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,10 @@
+var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-
-/*
 function addElementToBeginningOfArray(array, element) {
-  array.unshift(element);
-  return 
+  
 }
-*/
+
+function destructivelyAddElementToBeginningOfArray(array, element) {
+  array.unshift(element);
+  return array;
+}
